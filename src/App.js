fa661c3d9f8ef74a954bf0c2da4366f9c87ccd86
Dinu-Senal <60167570+Dinu-Senal@users.noreply.github.com>
2021-02-13 08:40:26 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>My Portfolio</h1>
+      <h2>Author: Dinu Senal Sendanayake</h2>
+    </div>
+  );
+}
+
+export default App;
