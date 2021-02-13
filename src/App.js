b6ca95bf4c80c647395c5d00c8f43c21e86fa97f
Dinu-Senal <60167570+Dinu-Senal@users.noreply.github.com>
2021-02-13 08:47:51 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <h1>My Portfolio</h1>
-      <h2>Author: Dinu Senal Sendanayake</h2>
+      <h2>Author: Dinu Senal</h2>
     </div>
   );
 }
